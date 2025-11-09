bf779931444432f4b9019a176bede53f975f99bf
auliahanifan <aulia@hanifan.id>
2025-11-09 10:33:40 +0700

    fix: dependency

--- src/transcription/transcription.module.ts
+++ src/transcription/transcription.module.ts
@@ -3,9 +3,10 @@ import { TranscriptionGateway } from './transcription.gateway';
 import { TranscriptionService } from './transcription.service';
 import { LoggerModule } from '../common/logger/logger.module';
 import { DatabaseModule } from '../database/database.module';
+import { SubscriptionModule } from '../subscription/subscription.module';
 
 @Module({
-  imports: [LoggerModule, DatabaseModule],
+  imports: [LoggerModule, DatabaseModule, SubscriptionModule],
   controllers: [],
   providers: [TranscriptionGateway, TranscriptionService],
 })
